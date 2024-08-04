4257b18db72e14b73747d0b3f1472d48b8b29033
Mahmudul Hasan <mhasancse15@gmail.com>
2024-08-04 11:00:40 +0600

    dependency setup

---
+++ app/src/main/java/com/mhasancse15/recipesearchcomposecleanapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mhasancse15.recipesearchcomposecleanapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mhasancse15.recipesearchcomposecleanapp.ui.theme.RecipeSearchComposeCleanAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecipeSearchComposeCleanAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RecipeSearchComposeCleanAppTheme {
+        Greeting("Android")
+    }
+}
